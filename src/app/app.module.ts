25cfdac5f86de7b3d56e2489a0929477b2822da3
fridolinho <fridolinho@gmail.com>
2019-11-11 13:58:50 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { TopComponent } from './components/top/top.component';
+import { SectionComponent } from './components/section/section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    TopComponent,
+    SectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
